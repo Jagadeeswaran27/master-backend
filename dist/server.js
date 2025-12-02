19246529293255e97cf9c537c64c432b51a9d4c4
Jagadeeswaran27 <jagadeeswaran2705@gmail.com>
2025-12-02 21:11:01 +0530

    eb deploy

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,5 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
 import app from "./app.js";
 import logger from "./utils/logger.js";
 const PORT = process.env.PORT || "8081";
